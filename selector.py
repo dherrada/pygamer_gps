e1f688b7033aa10dec542b7811a58d7b4f2ea1d1
dherrada <dylan.herrada@gmail.com>
2019-08-16 15:30:33 -0400

    Now controlled by joysticks, and added farfromhome

--- selector.py
+++ selector.py
@@ -2,6 +2,8 @@ import time
 import board
 import busio
 import digitalio
+import analogio
+from math import pi,sqrt,sin,cos,atan2
 
 from gamepadshift import GamePadShift
 
@@ -22,49 +24,88 @@ pad = GamePadShift(digitalio.DigitalInOut(board.BUTTON_CLOCK),
                    digitalio.DigitalInOut(board.BUTTON_OUT),
                    digitalio.DigitalInOut(board.BUTTON_LATCH))
 
+joystick_x = analogio.AnalogIn(board.JOYSTICK_X)
+joystick_y = analogio.AnalogIn(board.JOYSTICK_Y)
+
 pressed = pad.get_pressed()
+select = 0
+b = 0
 
 last_print = time.monotonic()
 a = 0
+
+homelat = 38.888
+homelon = -77.115
+
+def haversine(lat1, lon1, lat2, lon2):
+
+     degree_to_rad = float(pi / 180.0)
+
+     d_lat = (lat2 - lat1) * degree_to_rad
+     d_lon = (lon2 - lon1) * degree_to_rad
+
+     a = pow(sin(d_lat / 2), 2) + cos(lat1 * degree_to_rad) * cos(lat2 * degree_to_rad) * pow(sin(d_lon / 2), 2    )
+     c = 2 * atan2(sqrt(a), sqrt(1 - a))
+     km = 6367 * c
+     mi = 3956 * c
+ 
+     return {"km":km, "miles":mi}
+
 while True:
     gps.update()
 
     current = time.monotonic()
-    if current - last_print >= 1.0:
+    if current - last_print >= 0.2:
         last_print = current
         if not gps.has_fix:
             print('Waiting for fix...')
             continue
+        if joystick_y.value > 37000 and a < 4:
+            a += 1
+        elif joystick_y.value < 28000 and a > 0:
+            a -= 1
+        if not b:
+            array = [' ', ' ', ' ', ' ', ' ']
+            array.insert(a, '*')
+            print("{0}Farfromhome\n{1}Longitude\n{2}Latitude\n{3}Datetime\n{4}Speed".format(array[0], array[1], array[2], array[3], array[4]))
+            print("\n\n")
+        if joystick_x.value > 37000:
+            b = 1
+        elif joystick_x.value < 28000:
+            b = 0
+        if b:
+            if a == 0:
+                # farfromhome
+                 dist = haversine(gps.latitude, gps.longitude, homelat, homelon)
+                 print("Lat: {0:.6f}".format(gps.latitude))
+                 print("Lon: {0:.6f}\n".format(gps.longitude))
+                 print("You are: {0:.3f} km\nfrom home\n\nAnd: {1:.3f} miles\nfrom home".format(dist["km"], dist["miles"]))
+ 
+            elif a == 1:
+                # longitude
+                print('Longitude: {0:.6f} \ndegrees'.format(gps.longitude))
+                print('\n\n\n\n\n')
+                
+            elif a == 2:
+                # latitude
+                print('Latitude: {0:.6f} \ndegrees'.format(gps.latitude))
+                print('\n\n\n\n\n')
 
-        pressed = pad.get_pressed()
-        if pressed:
-            a = pressed
-        if a == 8:
-            # longitude
-            print('Longitude: {0:.6f} \ndegrees'.format(gps.longitude))
-            print('\n\n\n\n\n')
-            
-        elif a == 4:
-            # latitude
-            print('Latitude: {0:.6f} \ndegrees'.format(gps.latitude))
-            print('\n\n\n\n\n')
+            elif a == 3:
+                # datetime
+                print('Fix timestamp: {}/{}/{} {:02}:{:02}:{:02}'.format(
+                    gps.timestamp_utc.tm_mon,   # Grab parts of the time from the
+                    gps.timestamp_utc.tm_mday,  # struct_time object that holds
+                    gps.timestamp_utc.tm_year,  # the fix time.  Note you might
+                    gps.timestamp_utc.tm_hour,  # not get all data like year, day,
+                    gps.timestamp_utc.tm_min,   # month!
+                    gps.timestamp_utc.tm_sec))
+                print('\n\n\n\n\n')
 
-        elif a == 2:
-            # datetime
-            print('Fix timestamp: {}/{}/{} {:02}:{:02}:{:02}'.format(
-                gps.timestamp_utc.tm_mon,   # Grab parts of the time from the
-                gps.timestamp_utc.tm_mday,  # struct_time object that holds
-                gps.timestamp_utc.tm_year,  # the fix time.  Note you might
-                gps.timestamp_utc.tm_hour,  # not get all data like year, day,
-                gps.timestamp_utc.tm_min,   # month!
-                gps.timestamp_utc.tm_sec))
-            print('\n\n\n\n\n')
-
-        elif a == 1:
-            # speed
-            if gps.speed_knots is not None:
-                print('Speed: {} knots'.format(gps.speed_knots))
-                print('\n\n\n\n\n\n')
-        
-        else:
-            print("Red for longitude\nYellow for latitude\nBlack for datetime\nWhite for speed\n\n\n\n")
+            elif a == 4:
+                # speed
+                if gps.speed_knots is not None:
+                    print('Speed: {} knots'.format(gps.speed_knots))
+                    print('Speed: {} mph'.format(gps.speed_knots*1.151))
+                    print('speed: {} kmh'.format(gps.speed_knots*1.852))
+                    print('\n\n\n\n')
